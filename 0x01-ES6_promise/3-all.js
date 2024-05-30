b2b75ec2bb0afa740023de33e02efd63aff93a14
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-05-30 20:03:55 +0100

    test2: 3-all - error logging

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,14 +4,16 @@ export default function handleProfileSignup() {
   const profileData = Promise.all([uploadPhoto(), createUser()]);
 
   profileData.then((values) => {
-    const profileDetails = [];
-    profileDetails.push(values[0].body);
-    profileDetails.push(values[1].firstName);
-    profileDetails.push(values[1].lastName);
-    console.log(profileDetails.join(' '));
-  }).catch((error) => {
-	throw new Error('Signup system offline');
-	console.error(error)
-  });
+    if (values) {
+      const profileDetails = [];
+      profileDetails.push(values[0].body);
+      profileDetails.push(values[1].firstName);
+      profileDetails.push(values[1].lastName);
+      console.log(profileDetails.join(' '));
+    } else {
+      throw new Error('Signup system offline');
+    }
+  }).catch((error) => console.log(error));
+
   return profileData;
 }
