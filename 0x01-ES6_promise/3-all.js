16977b3ce5f86938dbf3fa6335cd60e3495660b0
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-05-30 20:07:57 +0100

    test3: 3-all - simplify promise handling and error logging

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,19 +1,10 @@
-import { uploadPhoto, createUser } from './utils';
+import { uploadPhoto, createUser } from './utils'
 
 export default function handleProfileSignup() {
-  const profileData = Promise.all([uploadPhoto(), createUser()]);
-
-  profileData.then((values) => {
-    if (values) {
-      const profileDetails = [];
-      profileDetails.push(values[0].body);
-      profileDetails.push(values[1].firstName);
-      profileDetails.push(values[1].lastName);
-      console.log(profileDetails.join(' '));
-    } else {
-      throw new Error('Signup system offline');
-    }
-  }).catch((error) => console.log(error));
-
-  return profileData;
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
