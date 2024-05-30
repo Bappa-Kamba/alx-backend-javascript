1e8ac104c47595e56dc143123af43ca5115bfe71
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-05-30 20:42:43 +0100

    fix: 6-final-user - format returned array correctly

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,3 +1,4 @@
 import handleProfileSignup from './6-final-user';
 
-console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
+const res = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
+res.then((results) => console.log(results));
