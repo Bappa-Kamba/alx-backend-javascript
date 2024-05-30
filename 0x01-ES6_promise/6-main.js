619ba0e49595979089572071834d527cdfe4bfbb
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-05-30 21:00:38 +0100

    fix: 6-final-user - refactored code and made it more readable, returned correct array

--- 0x01-ES6_promise/6-main.js
+++ 0x01-ES6_promise/6-main.js
@@ -1,4 +1,3 @@
 import handleProfileSignup from './6-final-user';
 
-const res = handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg");
-res.then((results) => console.log(results));
+console.log(handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg"));
