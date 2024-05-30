4e670967c9a72d4088c9c5e47faf7af8c0dc1882
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-05-30 12:53:27 +0100

    fix2: 2-then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,12 @@
 export default function handleResponseFromAPI(promise) {
-  promise
-    .then((result) => ({ code : 200, body : 'success' }))
-    .catch(() => {return new Error();})
-    .finally(() => console.log('Got a response from the API'));
-  return promise;
+    promise
+        .then((result) => {
+            return { status: 200, body: 'success' };
+        })
+        .catch((error) => {
+            return new Error();
+        })
+        .finally(() => {
+            console.log('Got a response from the API');
+        });
 }
