cba68ea4e98ce4024609ce7c889f01acb0e54f29
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-04 22:28:56 +0100

    file: 0-get_list_students

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+export default function getListStudents() {
+  const students = [];
+  students.push({
+    firstName: 'Guilleume',
+    id: 1,
+    location: 'San Francisco',
+  });
+  students.push({
+    firstName: 'James',
+    id: 2,
+    location: 'Columbia',
+  });
+  students.push({
+    firstName: 'Serena',
+    id: 5,
+    location: 'San Francisco',
+  });
+
+  return students;
+}
