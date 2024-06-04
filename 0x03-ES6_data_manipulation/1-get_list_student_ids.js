2d2ff78f2de5203ac447418b04266a6992311ab4
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-04 22:59:35 +0100

    update: 1-get_list_student_ids - changed variable name

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,5 +1,5 @@
-export default function getListStudentIds(studentsArray){
-	if (Array.isArray(studentsArray) == false)
+export default function getListStudentIds(students){
+	if (Array.isArray(students) == false)
 		return [];
 	 return students.map(student => student.id);
 }
