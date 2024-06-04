12437fe7b659c2c0f486fb583314a4148a1b0648
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-04 22:46:09 +0100

    file: 1-get_list_student_ids

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,4 +1,6 @@
 export default function getListStudentIds(studentsArray){
-	
-
+	if (Array.isArray(studentsArray) == false)
+		return [];
+	const ids = studentsArray.map((list) => list['id']);
+	return ids;
 }
