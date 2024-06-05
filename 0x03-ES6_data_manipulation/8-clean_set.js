4bbada0d4c8fa2336932429b16672b9586ba70ee
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-06-05 08:36:57 +0100

    ckean up

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,5 @@
 export default function cleanSet(set, startString) {
-  if (typeof startString !== 'string' || startString.length === 0) {
+  if (typeof stratString !== 'string' || startString.length === 0) {
     return '';
   }
 
