1ff0e0ad7e447d4ae58605d88a3a9332fdb22eb5
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-31 17:34:43 +0100

    :zap: implemented server with multiple routes

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+	res.setHeader('Content-Type', 'text/plain');
+	
+	if (req.url === '/') {
+		res.writeHead(200);
+		res.end('Hello Holberton School!');
+	} else if (req.url === '/students') {
+		res.statusCode = 200;
+
+		const dbPath = process.argv[2];
+		res.write('This is the list of our students\n');
+
+		countStudents(dbPath)
+			.then((data) => {
+				res.write(data);
+				res.end();
+			})
+			.catch ((error) => {
+				res.write('Cannot load the database\n')
+				res.end();
+			});
+	}
+});
+
+const port = 1245;
+app.listen(port, () => {
+	console.log(`Server is running at http://localhost:${port}/`);
+});
+
+module.exports = app;
