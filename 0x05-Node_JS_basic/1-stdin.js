79ffe59bd9c2c92f72744bcd5c856750470cf28b
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-31 14:49:16 +0100

    :zap: refactored code to use process.stdin and process.stdout

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,25 +1,13 @@
-const readline = require('readline');
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
-// Create an interface for reading input and output
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+process.stdin.on('readable', () => {
+	const line = process.stdin.read();
+
+	if (line)
+		process.stdout.write(`Your name is: ${line}`);
 });
 
-// Display the initial message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('end', () => {
+	process.stdout.write('This important software is now closing\n');
 
-// Listen for the user's input
-rl.question('', (input) => {
-  // Output the user's name
-  console.log(`Your name is: ${input}`);
-
-  // Display the closing message and close the interface
-  rl.close();
-});
-
-// When the readline interface is closed, display the closing message
-rl.on('close', () => {
-  console.log('This important software is now closing');
-  process.exit(0);
 });
