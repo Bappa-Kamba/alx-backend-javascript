b34b3000bcc866e438d65805e8a94ef9bf6c5487
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-31 14:40:48 +0100

    :zap: redid code

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,8 +2,8 @@ const readline = require('readline');
 
 // Create an interface for reading input and output
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout,
 });
 
 // Display the initial message
@@ -11,15 +11,15 @@ console.log('Welcome to Holberton School, what is your name?');
 
 // Listen for the user's input
 rl.question('', (input) => {
-    // Output the user's name
-    console.log(`Your name is: ${input}`);
+  // Output the user's name
+  console.log(`Your name is: ${input}`);
 
-    // Display the closing message and close the interface
-    rl.close();
+  // Display the closing message and close the interface
+  rl.close();
 });
 
 // When the readline interface is closed, display the closing message
 rl.on('close', () => {
-    console.log('This important software is now closing');
-    process.exit(0);
+  console.log('This important software is now closing');
+  process.exit(0);
 });
