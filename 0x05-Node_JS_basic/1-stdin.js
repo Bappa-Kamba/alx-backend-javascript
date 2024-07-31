57f455c93c2e47f810fa1734ff0a8c6ca350cdc0
Bappa-Kamba <bappahmuhammad18@gmail.com>
2024-07-31 14:55:14 +0100

    :shirt:

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,11 @@
-process.stdout.write("Welcome to Holberton School, what is your name?\n");
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
-	const line = process.stdin.read();
+  const line = process.stdin.read();
 
-	if (line)
-		process.stdout.write(`Your name is: ${line}`);
+  if (line) process.stdout.write(`Your name is: ${line}`);
 });
 
 process.stdin.on('end', () => {
-	process.stdout.write('This important software is now closing\n');
-
+  process.stdout.write('This important software is now closing\n');
 });
